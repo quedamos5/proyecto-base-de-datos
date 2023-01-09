7679cbdeeebdbffb86acb5a9e1462901757145b8
Andres Izquierdo <andres.izbri@gmail.com>
2023-01-09 13:44:59 +0000

    Formateo del código SQL

--- consultas.sql
+++ consultas.sql
@@ -1,49 +1,127 @@
 -- Usando la base de datos steam
-use steam;
+USE steam;
+
 go
+
 -- 1. Lista todas las columnas de la tabla.
-select COLUMN_NAME
-from INFORMATION_SCHEMA.COLUMNS
-where TABLE_NAME = 'juegos';
+SELECT column_name
+FROM   information_schema.columns
+WHERE  table_name = 'juegos';
+
 go
+
 -- 2. Lista todos los diferentes géneros, eliminando duplicados.
-select distinct CAST(Genre as varchar(1000)) as Genero
-from juegos;
+SELECT DISTINCT Cast(genre AS VARCHAR(1000)) AS Genero
+FROM   juegos;
+
 go
+
 -- 3. Lista únicamente aquellos juegos que puedan ser jugados únicamente en Linux
-select Nombre, PlatformLinux, PlatformMac, PlatformWindows
-from juegos
-where PlatformLinux like 'True' and PlatformMac like 'False' and PlatformWindows like 'False';
+SELECT nombre,
+       platformlinux,
+       platformmac,
+       platformwindows
+FROM   juegos
+WHERE  platformlinux LIKE 'True'
+       AND platformmac LIKE 'False'
+       AND platformwindows LIKE 'False';
+
 go
+
 -- 4.Lista todos los juegos de rol, ordenador por nota.
-select CAST(Nombre as varchar(1000)) as Nombre, Metacritic
-from juegos
-where Genre like '%RPG%'
-order by Metacritic desc;
+SELECT Cast(nombre AS VARCHAR(1000)) AS Nombre,
+       metacritic
+FROM   juegos
+WHERE  genre LIKE '%RPG%'
+ORDER  BY metacritic DESC;
+
 go
+
 -- 5.Lista todos aquellos juegos que puedan ser jugados en Español pero no en Frances
-select Nombre, SupportedLanguages
-from juegos
-where SupportedLanguages not like '%French%' and SupportedLanguages like '%Spanish%';
+SELECT nombre,
+       supportedlanguages
+FROM   juegos
+WHERE  supportedlanguages NOT LIKE '%French%'
+       AND supportedlanguages LIKE '%Spanish%';
+
 go
+
 -- 6.Lista el nombre del juego, su precio inicial, su precio final y la diferencia entre ambos en una nueva columna.
-select Nombre, PriceInitial, PriceFinal, (PriceInitial - PriceFinal) as Diferencia
-from juegos
-order by Diferencia desc;
+SELECT nombre,
+       priceinitial,
+       pricefinal,
+       ( priceinitial - pricefinal ) AS Diferencia
+FROM   juegos
+ORDER  BY diferencia DESC;
+
 go
+
 -- 7.¿Cuántos juegos hay del siglo pasado?
-select CAST(Nombre as varchar(1000)) as Nombre, ReleaseDate  
-from juegos
-where ReleaseDate like '[^2]%';
+SELECT Cast(nombre AS VARCHAR(1000)) AS Nombre,
+       releasedate
+FROM   juegos
+WHERE  releasedate LIKE '[^2]%';
+
 go
+
 -- 8.Devuelve todos los juegos que terminen o en N o en O.
+SELECT nombre
+FROM   juegos
+WHERE  nombre LIKE '%n'
+        OR nombre LIKE '%o';
+
+go
 
 -- 9.Devuelve todos los juegos que empiecen por A y que se puedan jugar con mando.
+SELECT nombre
+FROM   juegos
+WHERE  nombre LIKE 'a%'
+       AND controllersupport = 'True';
+
+go
+
 -- 10.Devuelve todos aquellos juegos que estén entre el 2000 y el 2010.
+SELECT nombre,
+       releasedate
+FROM   juegos
+WHERE  releasedate BETWEEN 2000 AND 2010;
+
+go
+
 -- 11.Devuelve todos los juegos que sean de la saga Final Fantasy.
+SELECT nombre
+FROM   juegos
+WHERE  nombre LIKE '%Final Fantasy%';
+
+go
+
 -- 12.Devuelve todos los juegos que sean de deportes y haya trabajado más de 3 desarrolladores.
+SELECT nombre,
+       genre,
+       developercount
+FROM   juegos
+WHERE  genre LIKE '%sport%'
+       AND developercount > 3;
+
+go
+
 -- 13.¿Cuántos juegos hay asociados a cada categoría?
+SELECT genre,
+       Count(nombre) AS CantidadJuegos
+FROM   juegos
+GROUP  BY genre
+ORDER  BY cantidadjuegos DESC;
+
+go
+
 -- 14.¿Cuántos juegos se han sacado en cada año?
+SELECT releasedate,
+       Count(nombre) AS CantidadJuegos
+FROM   juegos
+GROUP  BY releasedate
+ORDER  BY releasedate;
+
+go
 -- 15.En base a la consulta anterior, devuelve aquellos años en los que la media de puntuación esté entre un 6 y un 8,
 -- 16.¿Cuál es la máxima, mínima y puntuación mínima por género?
 -- 17.Usando LIMIT, devuelve el top 10 de juegos con mayor puntuación del 2012.
